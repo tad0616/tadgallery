8f4920a0f5247ac31c8087de69e6e63e264045a9
Tad <tad0616@gmail.com>
2016-04-13 00:00:29 +0800

    修正相簿區塊會導致版面不正常的問題

--- blocks/tadgallery_carousel.php
+++ blocks/tadgallery_carousel.php
@@ -67,7 +67,7 @@ function tadgallery_carousel_show($options)
 
     //引入TadTools的jquery
     if (!file_exists(XOOPS_ROOT_PATH . "/modules/tadtools/jquery.php")) {
-        redirect_header("http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50", 3, _TAD_NEED_TADTOOLS);
+        redirect_header("http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1", 3, _TAD_NEED_TADTOOLS);
     }
     include_once XOOPS_ROOT_PATH . "/modules/tadtools/jquery.php";
 
--- xoops_version.php
+++ xoops_version.php
@@ -2,7 +2,7 @@
 $modversion = array();
 
 $modversion['name']        = _MI_TADGAL_NAME;
-$modversion['version']     = 3.54;
+$modversion['version']     = 3.55;
 $modversion['description'] = _MI_TADGAL_DESC;
 $modversion['author']      = _MI_TADGAL_AUTHOR;
 $modversion['credits']     = _MI_TADGAL_CREDITS;
@@ -12,7 +12,7 @@ $modversion['license_url'] = 'www.gnu.org/licenses/gpl-2.0.html/';
 $modversion['image']       = "images/logo_{$xoopsConfig['language']}.png";
 $modversion['dirname']     = basename(__DIR__);
 
-$modversion['release_date']        = '2015/06/06';
+$modversion['release_date']        = '2016/04/12';
 $modversion['module_website_url']  = 'http://tad0616.net/';
 $modversion['module_website_name'] = _MI_TAD_WEB;
 $modversion['module_status']       = 'release';
