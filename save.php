6462ddcf5f5a9c789ccd79211cc6ec8a83ee3a55
Tad <tad0616@gmail.com>
2016-09-01 12:34:08 +0800

    安全性過濾

--- save.php
+++ save.php
@@ -1,6 +1,11 @@
 <?php
 include "header.php";
-$value = str_replace("<br>", '', $_POST['value']);
-$sql   = "update " . $xoopsDB->prefix("tad_gallery_cate") . " set `content`='{$value}' where csn='{$_POST['csn']}'";
+
+include_once $GLOBALS['xoops']->path('/modules/system/include/functions.php');
+$value = system_CleanVars($_POST, 'value', '', 'string');
+$csn   = system_CleanVars($_POST, 'csn', '', 'int');
+
+$value = str_replace("<br>", '', $value);
+$sql   = "update " . $xoopsDB->prefix("tad_gallery_cate") . " set `content`='{$value}' where csn='{$csn}'";
 $xoopsDB->queryF($sql);
-echo $_POST['value'];
+echo $value;
