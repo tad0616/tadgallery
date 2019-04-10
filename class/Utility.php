b210b1eddfabc8c8320a6ad515cc7239e343c28f
mambax7 <mambax7@gmail.com>
2019-04-10 11:07:25 -0400

    utility
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ class/Utility.php
@@ -0,0 +1,231 @@
+<?php namespace XoopsModules\Tadgallery;
+
+/*
+ Utility Class Definition
+
+ You may not change or alter any portion of this comment or credits of
+ supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit
+ authors.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @license      http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @copyright    https://xoops.org 2001-2017 &copy; XOOPS Project
+ * @author       Mamba <mambax7@gmail.com>
+ */
+
+
+/**
+ * Class Utility
+ */
+class Utility
+{
+    //建立目錄
+    public static function mk_dir($dir = "")
+    {
+        //若無目錄名稱秀出警告訊息
+        if (empty($dir)) {
+            return;
+        }
+
+        //若目錄不存在的話建立目錄
+        if (!is_dir($dir)) {
+            umask(000);
+            //若建立失敗秀出警告訊息
+            if (!mkdir($dir, 0777) && !is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+            }
+        }
+    }
+
+    //刪除目錄
+    public static function delete_directory($dirname)
+    {
+        if (is_dir($dirname)) {
+            $dir_handle = opendir($dirname);
+        }
+
+        if (!$dir_handle) {
+            return false;
+        }
+
+        while ($file = readdir($dir_handle)) {
+            if ($file != "." && $file != "..") {
+                if (!is_dir($dirname . "/" . $file)) {
+                    unlink($dirname . "/" . $file);
+                } else {
+                    self::delete_directory($dirname . '/' . $file);
+                }
+
+            }
+        }
+        closedir($dir_handle);
+        rmdir($dirname);
+        return true;
+    }
+
+    //拷貝目錄
+    public static function full_copy($source = "", $target = "")
+    {
+        if (is_dir($source)) {
+            @mkdir($target);
+            $d = dir($source);
+            while (false !== ($entry = $d->read())) {
+                if ($entry == '.' || $entry == '..') {
+                    continue;
+                }
+
+                $Entry = $source . '/' . $entry;
+                if (is_dir($Entry)) {
+                    self::full_copy($Entry, $target . '/' . $entry);
+                    continue;
+                }
+                copy($Entry, $target . '/' . $entry);
+            }
+            $d->close();
+        } else {
+            copy($source, $target);
+        }
+    }
+
+    public static function rename_win($oldfile, $newfile)
+    {
+        if (!rename($oldfile, $newfile)) {
+            if (copy($oldfile, $newfile)) {
+                unlink($oldfile);
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    //新增排序欄位
+    public static function chk_chk9()
+    {
+        global $xoopsDB;
+        $sql    = "SELECT count(`photo_sort`) FROM " . $xoopsDB->prefix("tad_gallery");
+        $result = $xoopsDB->query($sql);
+        if (empty($result)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function go_update9()
+    {
+        global $xoopsDB;
+        $sql = "ALTER TABLE " . $xoopsDB->prefix("tad_gallery") . " ADD `photo_sort` SMALLINT(5) UNSIGNED NOT NULL";
+        $xoopsDB->queryF($sql) or redirect_header(XOOPS_URL . "/modules/system/admin.php?fct=modulesadmin", 30, $xoopsDB->error());
+    }
+
+    public static function go_update10()
+    {
+        global $xoopsDB;
+        $sql = "update " . $xoopsDB->prefix("tad_gallery_cate") . " set show_mode='normal' where show_mode='thubm' or show_mode='slideshow' or show_mode='3d'";
+        $xoopsDB->queryF($sql) or redirect_header(XOOPS_URL . "/modules/system/admin.php?fct=modulesadmin", 30, $xoopsDB->error());
+    }
+
+    //新增說明欄位
+    public static function chk_chk11()
+    {
+        global $xoopsDB;
+        $sql    = "SELECT count(`content`) FROM " . $xoopsDB->prefix("tad_gallery_cate");
+        $result = $xoopsDB->query($sql);
+        if (empty($result)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function go_update11()
+    {
+        global $xoopsDB;
+        $sql = "ALTER TABLE " . $xoopsDB->prefix("tad_gallery_cate") . " ADD `content` TEXT NOT NULL AFTER `title`";
+        $xoopsDB->queryF($sql) or redirect_header(XOOPS_URL . "/modules/system/admin.php?fct=modulesadmin", 30, $xoopsDB->error());
+    }
+
+    //新增是否啟用欄位
+    public static function chk_chk12()
+    {
+        global $xoopsDB;
+        $sql    = "SELECT count(`enable`) FROM " . $xoopsDB->prefix("tad_gallery_cate");
+        $result = $xoopsDB->query($sql);
+        if (empty($result)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function go_update12()
+    {
+        global $xoopsDB;
+        $sql = "ALTER TABLE " . $xoopsDB->prefix("tad_gallery_cate") . " ADD `enable` ENUM('1','0') NOT NULL DEFAULT '1'";
+        $xoopsDB->queryF($sql) or redirect_header(XOOPS_URL . "/modules/system/admin.php?fct=modulesadmin", 30, $xoopsDB->error());
+    }
+
+    //刪除錯誤的重複欄位及樣板檔
+    public static function chk_tadgallery_block()
+    {
+        global $xoopsDB;
+        //die(var_export($xoopsConfig));
+        include XOOPS_ROOT_PATH . '/modules/tadgallery/xoops_version.php';
+
+        //先找出該有的區塊以及對應樣板
+        foreach ($modversion['blocks'] as $i => $block) {
+            $show_func                = $block['show_func'];
+            $tpl_file_arr[$show_func] = $block['template'];
+            $tpl_desc_arr[$show_func] = $block['description'];
+        }
+
+        //找出目前所有的樣板檔
+        $sql    = "SELECT bid,name,visible,show_func,template FROM `" . $xoopsDB->prefix("newblocks") . "` WHERE `dirname` = 'tadgallery' ORDER BY `func_num`";
+        $result = $xoopsDB->query($sql);
+        while (list($bid, $name, $visible, $show_func, $template) = $xoopsDB->fetchRow($result)) {
+            //假如現有的區塊和樣板對不上就刪掉
+            if ($template != $tpl_file_arr[$show_func]) {
+                $sql = "delete from " . $xoopsDB->prefix("newblocks") . " where bid='{$bid}'";
+                $xoopsDB->queryF($sql);
+
+                //連同樣板以及樣板實體檔案也要刪掉
+                $sql = "delete from " . $xoopsDB->prefix("tplfile") . " as a left join " . $xoopsDB->prefix("tplsource") . "  as b on a.tpl_id=b.tpl_id where a.tpl_refid='$bid' and a.tpl_module='tadgallery' and a.tpl_type='block'";
+                $xoopsDB->queryF($sql);
+            } else {
+                $sql = "update " . $xoopsDB->prefix("tplfile") . " set tpl_file='{$template}' , tpl_desc='{$tpl_desc_arr[$show_func]}' where tpl_refid='{$bid}'";
+                $xoopsDB->queryF($sql);
+            }
+        }
+
+    }
+
+    //新增360欄位
+    public static function chk_chk13()
+    {
+        global $xoopsDB;
+        $sql    = "SELECT count(`is360`) FROM " . $xoopsDB->prefix("tad_gallery");
+        $result = $xoopsDB->query($sql);
+        if (empty($result)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function go_update13()
+    {
+        global $xoopsDB;
+        $sql = "ALTER TABLE " . $xoopsDB->prefix("tad_gallery") . " ADD `is360` ENUM('0','1') NOT NULL DEFAULT '0'";
+        $xoopsDB->queryF($sql) or redirect_header(XOOPS_URL . "/modules/system/admin.php?fct=modulesadmin", 30, $xoopsDB->error());
+    }
+
+
+}
