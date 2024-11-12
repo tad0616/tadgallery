47f4cfe24bd3b95e8cd7b4ace729ccc54084768c
tad0616 <tad0616@gmail.com>
2024-11-12 16:25:05 +0800

    簡化後台選單陣列，修改釋出日期

--- admin/menu.php
+++ admin/menu.php
@@ -1,21 +1,13 @@
 <?php
-$adminmenu = [];
-$icon_dir = '2.6' === mb_substr(XOOPS_VERSION, 6, 3) ? '' : 'images/';
-
-$i = 1;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_HOME;
-$adminmenu[$i]['link'] = 'admin/index.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_HOME_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/home.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADGAL_ADMENU1;
-$adminmenu[$i]['link'] = 'admin/main.php';
-$adminmenu[$i]['desc'] = _MI_TADGAL_ADMENU1;
-$adminmenu[$i]['icon'] = 'images/admin/pictures.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_ABOUT;
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_ABOUT_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu = [
+    [
+        'title' => _MI_TADGAL_ADMENU1,
+        'link' => 'admin/main.php',
+        'icon' => 'images/admin/pictures.png',
+    ],
+    [
+        'title' => _MI_TAD_ADMIN_ABOUT,
+        'link' => 'admin/about.php',
+        'icon' => 'images/admin/about.png',
+    ],
+];
